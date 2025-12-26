8c0b9cbc3552288ee3bd7b6c277be9d63045e691
yuubc <yuna8.ca@gmail.com>
2025-12-26 18:41:36 +0900

    feat: ProductDetailPage created

--- lib/presentation/screens/product_detail_page.dart
+++ lib/presentation/screens/product_detail_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ProductDetailPage extends StatelessWidget {
-  const ProductDetailPage({super.key});
+class ProductDetailPage extends StatefulWidget {
+  final Product product;
+
+  const ProductDetailPage({Key? key, required this.product}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('상품 상세')),
-      body: const Center(child: Text('Product Detail Page')),
-    );
-  }
+  State<ProductDetailPage> createState() => _ProductDetailPageState();
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('상품 상세')),
+    body: const Center(child: Text('Product Detail Page')),
+  );
 }
