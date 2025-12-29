5298b768ae96d62fb70613710b9810a82fe2dda3
정유나_flutter_9기_창업 <qpla2424@gmail.com>
2025-12-29 16:15:53 +0900

    Update product_detail_page.dart

--- lib/presentation/screens/product_detail_page.dart
+++ lib/presentation/screens/product_detail_page.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:shopping_mall_project/data/models/product_entity.dart';
 
-class ProductDetailPage extends StatefulWidget{
-const ProductDetailPage({
-  super.key,
-  required this.product,
-});
-
-final ProductEntity product;
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-}
